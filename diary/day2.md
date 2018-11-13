f414a0e3a4a100e112e6bbebc2500153ad90d924
谷口　幸宏 <yukihiro@ytmbp.local>
2018-11-14 00:11:05 +0900

    commit_181113_pm

--- diary/day2.md
+++ diary/day2.md
@@ -20,10 +20,71 @@ def relu(x):
 return np.maximum(0, x) #入力値の大きい方を出力  
 ->入力が0を超えていればそのまま、0以下ならば0を出力
 
-x = numpy.array([1, 2, 3, 4])
+x = numpy.array([1, 2, 3, 4])  
 numpy.ndim(x) #次元数  
 -> 1
 
 行列の積  
 np.dot(A, B)  
 -> Aの列数とBの列数を一致させる
+
+出力層の活性化関数  
+回帰問題 : 恒等関数  
+分類問題 : ソフトマックス関数
+
+回帰問題  
+例) 人の写った画像からその人の体重を予測する
+
+
+ソフトマックス関数  
+def softmax(a):  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+exp_a = numpy.exp(a)  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+sum_exp_a = numpy.sum(exp_a)  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+y = exp_a / sum_exp_a  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+return y
+
+オーバーフロー  
+-> 数には有効桁数があり、大きすぎる値は表現できない
+
+
+ソフトマックス関数(ソフトマックス関数対策)  
+def softmax(a):  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+c = np.max(a)  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+exp_a = numpy.exp(a - c)  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+sum_exp_a = numpy.sum(exp_a)  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+y = exp_a / sum_exp_a  
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+return y
+
+ソフトマックス関数の特徴  
+1. 出力が0から1.0の実数になる
+2. 出力の総和が1になる  
+->出力を確立として解釈できる  
+->分類問題に適する
+
+numpy.argmax(y)  
+->最も大きな要素のインデックスを取得
+
+Normalaize : 正規化  
+-> データをある決められた範囲に変換する処理
+
+前処理  
+-> 入力データに対して決められた変換を行うこと
+
+range(start, end, step)  
+list( range(0, 10, 3) )  
+-> [0, 3, 6, 9]
+
+y = numpy.array([1, 2, 1, 0])  
+t = numpy.array([1, 2, 0, 0])  
+print(y == t) #[True True False True]  
+numpy.sum(y == t)  
+-> 3
--- script/chapter3.py
+++ script/chapter3.py
@@ -90,3 +90,214 @@ print(W)
 W.shape
 Y = np.dot(X, W)
 print(Y)
+
+#3.4.2 各層における信号伝達の実装
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+
+Z1 = sigmoid(A1)
+
+print(A1)
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+def identity_function(x):
+    return x
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)
+
+#3.4.3 3層ニューラルネットワークの実装まとめ
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
+
+#3.5.1 恒等関数とソフトマックス関数
+a = np.array([0.3, 2.9, 4.0])
+exp_a = np.exp(a)
+print(exp_a)
+
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+
+y = exp_a / sum_exp_a
+print(y)
+
+def sotmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+#3.5.2 ソフトマックス関数の実装上の注意
+a = np.array([1010, 1000, 990])
+np.exp(a) / np.sum(np.exp(a))
+c = np.max(a)
+a - c
+
+np.exp(a - c) / np.sum(np.exp(a - c))
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+#3.5.3 ソフトマックス関数の特徴
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)
+np.sum(y)
+
+#3.5.4 出力層のニューロンの数
+import sys, os
+os.getcwd()
+os.chdir('script')
+sys.path.append(os.pardir)
+from others.mnist import load_mnist
+
+#3.6.1 MNISTデータセット
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten = True, normalize = False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
+
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    plt.imshow(pil_img)
+
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+img_show(img)
+
+#3.6.2 ニューラルネットワークの推論処理
+import pickle
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize = True, flatten = True, one_hot_label = False)
+    return x_test, t_test
+
+def init_network():
+    with open("../others/sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print(f"Accuracy : {str(float(accuracy_cnt) / len(x))}")
+
+#3.6.3 バッチ処理
+x, _ = get_data()
+network = init_network()
+W1, W2, W3 = network['W1'], network['W2'], network['W3']
+
+x.shape
+x[0].shape
+W1.shape
+W2.shape
+W3.shape
+
+#バッチ処理実装
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis = 1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print(f"Accuracy : {str(float(accuracy_cnt) / len(x))}")
+
+
+x = np.array([[0.1, 0.8, 0.1], [0.3, 0.1, 0.6], [0.2, 0.5, 0.3], [0.8, 0.1, 0.1]])
+y = np.argmax(x, axis = 1)
+print(y)
+
+y = np.array([1, 2, 1, 0])
+t = np.array([1, 2, 0, 0])
+print(y == t)
+np.sum(y == t)
