d0852b1b73b42bf97193d8580fc7eb116a087533
谷口　幸宏 <yukihiro@ytmbp.local>
2018-11-16 00:31:14 +0900

    commit_181115_pm

--- script/chapter5.py
+++ script/chapter5.py
@@ -107,3 +107,206 @@ mask = (x <= 0)
 print(mask)
 x[mask] = 0
 print(x)
+
+#5.5.2 Sigmoid レイヤ
+def backward(self, dout):
+    dx = dout * (1.0 - self.out) * self.out
+
+    return dx
+
+#5.6.1 Affine レイヤ
+X = np.random.rand(2)
+W = np.random.rand(2, 3)
+B = np.random.rand(3)
+
+X.shape
+W.shape
+B.shape
+
+Y = np.dot(X, W) + B
+print(Y.shape)
+
+#5.6.2 バッチ版 Affine レイヤ
+X_dot_W = np.array([[0, 0, 0], [10, 10, 10]])
+B = np.array([1, 2, 3])
+X_dot_W
+X_dot_W + B
+
+dY = np.array([[1, 2, 3], [4, 5, 6]])
+dY
+dB = np.sum(dY, axis = 0)
+print(dB)
+
+class Affine:
+    def __init__(self, W, b):
+         self.W = W
+         self.b = b
+         self.x = None
+         self.dW = None
+         self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis = 0)
+
+        return dx
+
+
+#5.6.3 Softmax-with-Loss レイヤ
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y -self.t) / batch_size
+
+        return dx
+
+import sys, os
+os.getcwd()
+os.chdir('script')
+sys.path.append(os.pardir)
+from others.layers import *
+from others.gradient import numerical_gradient
+from collections import OrderedDict
+
+class TwoLayerNet :
+    def __init__(self, input_size, hidden_size, output_size,
+                 weight_init_std = 0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                               np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * \
+                               np.random.randn(input_size, hidden_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = \
+            Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = \
+            Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis = 1)
+        if t.ndim != 1 : t = np.argmax(t, axis = 1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+
+        return grads
+
+from others.mnist import load_mnist
+from others.two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize = True, one_hot_label = True)
+network = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10)
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+for key in  grad_numerical.keys():
+    diff = np.average( np.abs(grad_backprop[key] - grad_numerical[key]) )
+    print(key + ":" + str(diff))
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize = True, one_hot_label = True)
+network = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10)
+
+network = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10)
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grad = network.gradient(x_batch, t_batch)
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    if i % iter_per_epoch ==0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(train_acc, test_acc)
+        
