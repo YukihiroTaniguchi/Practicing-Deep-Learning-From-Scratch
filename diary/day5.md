fa852c8bde12f23f0c1b73a8017808bed380a8c2
谷口　幸宏 <yukihiro@ytmbp.local>
2018-11-17 17:27:25 +0900

    commit_181117_pm

--- diary/day5.md
+++ diary/day5.md
@@ -1,3 +1,59 @@
+### 181117(Sat)  
+p.165 - p.
+chapter6.py : L1 - L
+##### *Remember me*  
+確率的勾配降下法(SGD)  
+-> 欠点 : 関数の形状が等方的でないと非効率な経路で探索することになる
+
+モーメンタム(Momentum)  
+ボールが地面を転がるように動く  
+物体が勾配方向に力を受け、その力によって物体の速度が加算されるという物理法則
+```
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = Momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
+```
+
+AdaGrad  
+学習係数の減衰(lerning rate decay)を行う  
+->パラメータの要素の中でよく動いた(大きく更新された)要素は、学習係数が小さくなる
+```
+class AdaGrad:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+```
+
+Adam  
+Momentum と AdaGrad のハイブリッド
+
+隠れ層のアクティベーション  
+-> 活性化関数のあとの出力データ
+
+
+***
 Hello Im Kazuki Ueno.
 Its important to study reguraly!
 Stick to it!
---
+++ script/chapter6.py
@@ -0,0 +1,148 @@
+#6.1.2 SGD
+class SGD:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
+
+#6.1.2 Momentum
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = Momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
+
+#6.1.5 AdaGrad
+class AdaGrad:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+#6.2.1 重みの初期値を0にする?
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.random.randn(1000, 100)
+node_num = 100
+hidden_layer_size = 5
+activations = {}
+
+#重みの標準偏差 = 1
+for i in range(hidden_layer_size):
+    if i !=0:
+        x = activations[i-1]
+
+    w = np.random.randn(node_num, node_num) * 1
+
+    z = np.dot(x, w)
+    a = sigmoid(z)
+    activations[i] = a
+
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i + 1)
+    plt.title(str(i + 1) + "-layer")
+    plt.hist(a.flatten(), 30, range=(0,1))
+
+#重みの標準偏差 = 0.01
+for i in range(hidden_layer_size):
+    if i !=0:
+        x = activations[i-1]
+
+    w = np.random.randn(node_num, node_num) * 0.01
+
+    z = np.dot(x, w)
+    a = sigmoid(z)
+    activations[i] = a
+
+
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i + 1)
+    plt.title(str(i + 1) + "-layer")
+    plt.hist(a.flatten(), 30, range=(0,1))
+
+#Xavier の初期値
+for i in range(hidden_layer_size):
+    if i !=0:
+        x = activations[i-1]
+
+    w = np.random.randn(node_num, node_num) / np.sqrt(node_num)
+
+    z = np.dot(x, w)
+    a = sigmoid(z)
+    activations[i] = a
+
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i + 1)
+    plt.title(str(i + 1) + "-layer")
+    plt.hist(a.flatten(), 30, range=(0,1))
+
+import os, sys
+
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+from others.mnist import load_mnist
+from others.multi_layer_net import MultiLayerNet
+from others.optimizer import SGD
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+x_train = x_train[:300]
+t_train = t_train[:300]
+
+network = MultiLayerNet(input_size = 784, hidden_size_list = [100, 100, 100, 100, 100, 100], output_size =10)
+optimizer = SGD(lr = 0.01)
+
+max_epochs = 201
+train_size = x_train.shape[0]
+batch_size = 100
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+epoch_cnt = 0
+
+for i in range(1000000000):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grads = network.gradient(x_batch, t_batch)
+    optimizer.update(network.params, grads)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+
+        epoch_cnt += 1
+        if epoch_cnt >= max_epochs:
+            breakb
